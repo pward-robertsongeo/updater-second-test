db543a37b89ae6fe277c0db9c8604ce696602e52
pward-robertsongeo <109231004+pward-robertsongeo@users.noreply.github.com>
2022-07-18 16:16:35 +0100

    added auto updater

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <p> v1.0.0 </p>
   </div>
 </template>
 
--- src/background.ts
+++ src/background.ts
@@ -3,6 +3,7 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import { autoUpdater } from 'electron-updater'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
@@ -33,7 +34,9 @@ async function createWindow() {
     createProtocol('app')
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
+    
   }
+  autoUpdater.checkForUpdatesAndNotify()
 }
 
 // Quit when all windows are closed.
@@ -60,7 +63,7 @@ app.on('ready', async () => {
     try {
       await installExtension(VUEJS_DEVTOOLS)
     } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
+      console.error('Vue Devtools failed to install:', (e as any).toString())
     }
   }
   createWindow()
